606dc36727ee1748a139aaba34de103b09e9a8f2
Gnxsh12 <ganeshmorewad791@gmail.com>
2026-07-30 15:53:51 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 2.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "End"
+print(type(variable))
+variable = [1,2,3,4,5]
+print(type(variable))
