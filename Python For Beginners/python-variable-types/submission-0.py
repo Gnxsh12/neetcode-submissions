a06cbd88e74a1139245466c317f6ec0b1790b0c5
Gnxsh12 <ganeshmorewad791@gmail.com>
2026-07-30 15:49:42 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 10
+float_type = 5.6
+bool_type = True
+string_type = "Ganesh"
+list_type =[1,2,3,4,5]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
