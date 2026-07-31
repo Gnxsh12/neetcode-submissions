0e8cc84a53ad441a14e37037a9a353e011c7e2e3
Gnxsh12 <ganeshmorewad791@gmail.com>
2026-07-31 17:46:23 +0530

    Add: python-type-errors - submission-0

---
+++ Python For Beginners/python-type-errors/submission-0.py
@@ -0,0 +1,11 @@
+variable_1, variable_2 = 10, "10"
+variable_1, variable_2 = variable_2, variable_1
+
+# Don't modify the code below this line
+print(variable_1)
+print(type(variable_1))
+
+variable_1 = int(variable_1)
+
+print(variable_2)
+print(type(variable_2))
